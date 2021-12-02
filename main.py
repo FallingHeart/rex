c6d06f9610426dd24ed269ee62c57b8c41efce7b
rex nathan <ceo@bamo.tech>
2021-12-02 21:36:17 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+__version__ = '0.1.0'
+
+class str_manage():
+    def remove_space(string):
+        return string.replace(" ","")
+
+
+# def main():
+#   pass
+
+
+# if __name__ == '__main__':
+#     main()
+
+new_str = str_manage.remove_space("666 666")
+print(new_str)
